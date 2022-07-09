3e2a0651754089e9dbd16a1e11fac6c8ee4e2eee
Mee12321 <91099630+Mee12321@users.noreply.github.com>
2022-07-09 13:33:15 -0700

    Big changes to the code that will probably break it sorry if it does but the code was broken any way

--- Minibot/src/main/java/frc/team670/robot/commands/drive/DistanceDrive.java
+++ Minibot/src/main/java/frc/team670/robot/commands/drive/DistanceDrive.java
@@ -10,6 +10,7 @@ import frc.team670.robot.utils.Logger;
 public class DistanceDrive extends CommandBase {
 	
 	private double speedL, speedR, dist;
+	private final double LEFT_ENCODER_NUM;
 	private boolean correcting=false;
 	
 
@@ -24,6 +25,7 @@ public class DistanceDrive extends CommandBase {
 	public DistanceDrive(double distance_in, double lspeed, double rspeed, DriveBase driveBase) {
 		this.speedL = lspeed;
 		this.speedR = rspeed;
+		this.LEFT_ENCODER_NUM = 1.26421;
 		//this.seconds = seconds;
 		this.dist = distance_in;
 		this.driveBase = driveBase;
@@ -41,13 +43,13 @@ public class DistanceDrive extends CommandBase {
 	// Called repeatedly when this Command is scheduled to run
 	
 	public void execute() { 
-
 		Logger.consoleLog("LeftSpeed: %s Right Speed: %s DistanceT: %s TicksL: %s TicksR %s", 
-				speedL, speedR, getDistance(), (driveBase.getLeftEncoder().getTicks()/1.16421),-driveBase.getRightEncoder().getTicks());		
+				speedL, speedR, getDistance(), (driveBase.getLeftEncoder().getTicks()/LEFT_ENCODER_NUM),-driveBase.getRightEncoder().getTicks());
+				//speedL, speedR, getDistance(), (driveBase.getLeftEncoder().getTicks()),-driveBase.getRightEncoder().getTicks());		
+		
+		correcting = false;
 		if(!correcting){
 			driveBase.tankDrive(speedL, speedR);
-
-
 		}
 		correct();
 
@@ -60,7 +62,7 @@ public class DistanceDrive extends CommandBase {
 	public void end(boolean interrupted) {
 		driveBase.stop();
 		Logger.consoleLog("LeftSpeed: %s Right Speed: %s DistanceT: %s Ticks: %s", 
-				speedL, speedR, getDistance(), driveBase.getRightEncoder().getTicks());
+				speedL, speedR, getDistance(), Math.abs(driveBase.getRightEncoder().getTicks()));
 	}
 
 	// Called when another command which requires one or more of the same
@@ -72,63 +74,61 @@ public class DistanceDrive extends CommandBase {
 	// Checks that the wheels are driving at the same speed, corrects the speed
 	// so that the left/right are equal
 	public void correct() {
-		double currentTicksL = driveBase.getLeftEncoder().getTicks()/1.16421;
+		double currentTicksL = driveBase.getLeftEncoder().getTicks()/LEFT_ENCODER_NUM;
+		//double currentTicksL = driveBase.getLeftEncoder().getTicks();
 		double currentTicksR = -driveBase.getRightEncoder().getTicks();
 		
-		if (Math.abs(currentTicksL - currentTicksR) < 15)
+		if (Math.abs(currentTicksL - currentTicksR) < 5)
 			return;
 		
-		// else if (currentTicksL > currentTicksR)
-		// 		speedL -= ;
+		//else if (currentTicksL > currentTicksR)
+		// 		speedL -= 0.05;
 		
-		// else if (currentTicksL < currentTicksR)
+		//else if (currentTicksL < currentTicksR)
 		// 		speedR -= 0.05;
-	
-		else if (currentTicksL > currentTicksR){
-			speedL -= 0.025;
-			speedR += 0.025;
-		}
-			
 		else if (currentTicksL < currentTicksR){
-			speedR -= 0.025;
-			speedL += 0.025;
+			speedR -= 0.015;
+			speedL += 0.015;
 		}
-		speedL= speedL>1?1:speedL;
-		speedL= speedL<-1?-1:speedL;
+		else if (currentTicksL > currentTicksR){
+			speedL -= 0.015;
+			speedR += 0.015;
+		}
+		speedL = (speedL > 1) ? 1 : speedL;
+		speedL= (speedL < -1) ? -1 : speedL;
 
-		speedR= speedR>1?1:speedR;
-		speedR= speedR<-1?-1:speedR;
+		speedR = (speedR > 1) ? 1 : speedR;
+		speedR = (speedR < -1) ? -1: speedR;
 			
 	}		
 	
 	// Make this return true when this Command no longer needs to run execute()
 		@Override
 		public boolean isFinished() {
-			if(getDistance() > Math.abs(dist)){
-				double currentTicksL = driveBase.getLeftEncoder().getTicks()/1.16421;
-				double currentTicksR = -driveBase.getRightEncoder().getTicks();
-				if(Math.abs(currentTicksL - currentTicksR) <10){
+			// if(getDistance() > Math.abs(dist)){
+			// 	double currentTicksL = driveBase.getLeftEncoder().getTicks()/LEFT_ENCODER_NUM;
+			// 	//double currentTicksL = driveBase.getLeftEncoder().getTicks();
+			// 	double currentTicksR = -driveBase.getRightEncoder().getTicks();
+			// 	if (Math.abs(currentTicksL - currentTicksR) < 10) {
+			// 		return true;
+			// 	}
+			// 	if (currentTicksL > currentTicksR){
+			// 		driveBase.tankDrive(0, 1);
+			// 	}else{
+			// 		driveBase.tankDrive(1, 0);
 
-					return true;
-				}
-				if(currentTicksL>currentTicksR){
-					driveBase.tankDrive(0, 0.5);
-				}else{
-					driveBase.tankDrive(0.5, 0);
-
-				}
-				correcting=true;
-				return false;
-			}
-			return false;
-			// return getDistance() > Math.abs(dist);
+			// 	}
+			// 	correcting=true;
+			// 	return false;
+			// }
+			return getDistance() >= Math.abs(dist);
 			//return (this.error <= 1);
 		}
 		
 			
 		public double getDistance()
 		{
-			double distance = (driveBase.getRightEncoder().getDistance())/1;
+			double distance = driveBase.getRightEncoder().getDistance();
 			return Math.abs(distance);
 		}
 
